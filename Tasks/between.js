68da17850d401550a2f7fab3cad09ecfbd2f3ed1
Anastasiia Tkachenko <anastasiiatkk@gmail.com>
2024-01-13 21:50:13 +0000

    Update identifier's names in between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,18 +2,18 @@
 
 'use strict';
 
-const getvaluebetween = (str, p, s) => {
-  let i = str.indexOf(p);
-  if (i === -1) return '';
+const getvaluebetween = (str, prefix, suffix) => {
+  let prefixIndex = str.indexOf(prefix);
+  if (prefixIndex === -1) return '';
   else {
-    const k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
-      if (i === -1) {
+    const prefixEndIndex = prefixIndex + prefix.length;
+    str = str.substring(prefixEndIndex);
+    if (suffix) {
+      prefixIndex = str.indexOf(suffix);
+      if (prefixIndex === -1) {
         return '';
       } else {
-        str = str.substring(0, i);
+        str = str.substring(0, prefixIndex);
       }
     }
   }
