e5b745b16a296d2c1fce9762c65bc39e65e56fb0
Anastasiia Tkachenko <anastasiiatkk@gmail.com>
2024-01-14 15:05:28 +0000

    Add 'const' and 'let' before identifiers in ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -2,13 +2,13 @@
 
 'use strict';
 
-Parseip = (i) => {
-  a = [];
+const Parseip = (i) => {
+  const a = [];
   if (i === '') return;
   else {
-    B = i.split('.');
+    const B = i.split('.');
     if (B.length != 4) return;
-    j = 0;
+    let j = 0;
     for (const b of B) {
       a[j] = parseInt(b);
       if (isNaN(a[j])) return;
