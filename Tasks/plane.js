ccc429e0f8828af17ccee9ecd94545a5472a6dc8
Anastasiia Tkachenko <anastasiiatkk@gmail.com>
2024-01-14 13:58:13 +0000

    Add spaces and semicolons in plane.js

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,19 +2,20 @@
 
 'use strict';
 
-const const_plane = function(arr, res=[]) {
-  let j = 0; for(let i = 0, length = arr.length; i < length; i++){
+const const_plane = function(arr, res = []) {
+  let j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
+};
 
 module.exports = const_plane;
